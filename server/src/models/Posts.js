980e5e72fb9d4818b82d53a6cde19261a15a6816
d-lin <lind7@rpi.edu>
2023-11-05 04:08:01 -0500

    check

--- server/src/models/Posts.js
+++ server/src/models/Posts.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 const PostSchema = new mongoose.Schema({
   poster: {type: String, required: true, unique: true},
-  post_content: {type: String, required: true, unique: true}
+  post_content: {type: String, required: true, unique: true},
+  time_stamp: {type: Date, default: Date.now }
 })
 
 export const PostModel = mongoose.model("posts", PostSchema);
