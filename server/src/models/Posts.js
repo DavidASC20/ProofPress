19719b7af38806f450fab12ab3efdd113490d6af
d-lin <lind7@rpi.edu>
2023-11-05 06:14:27 -0500

    mongodb database is working

--- server/src/models/Posts.js
+++ server/src/models/Posts.js
@@ -1,9 +1,8 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
-  poster: {type: String, required: true, unique: true},
-  post_content: {type: String, required: true, unique: true},
-  time_stamp: {type: Date, default: Date.now }
-})
+  poster_address: {type: String, required: true},
+  post_content: {type: String, required: true}
+});
 
 export const PostModel = mongoose.model("posts", PostSchema);
--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,26 +1,14 @@
 import express from "express";
-import mongoose from "mongoose";
 import { PostModel } from "../models/Posts.js";
 
 const router = express.Router();
 
-router.get("/", async(req, res) => {
-  try {
-    const response = await PostModel.find({});
-    res.json(response);
-  } catch (err) {
-    res.json(err);
-  }
-});
+router.post("/create", async(req, res) => {
+  const {poster_address, post_content} = req.body;
 
-router.post("/", async(req, res) => {
-  const post = new PostModel(req.body);
-  try {
-    const response = await PostModel.find({});
-    res.json(response);
-  } catch (err) {
-    res.json(err);
-  }
+  const newPost = new PostModel({poster_address, post_content});
+  await newPost.save();
+  res.json({message: "postt registered successfully"});
 });
 
 export { router as postRouter };
