32a36990de12772d33e781d0767b12f8b8007d86
David Chong <djc@uilleann-pipes-407.dynamic2.rpi.edu>
2023-11-05 07:14:13 -0500

    adding all

--- contracts/PressStake.sol
+++ contracts/PressStake.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract NewsStaking{
     IERC20 public pressToken;
@@ -25,6 +25,7 @@ contract NewsStaking{
     uint256 private _nextArticleId;
     uint256 private _nextVerificationId;
     uint256 private _nextDisputeId;
+    address private owner;
 
     // Events
     event ArticleSubmitted(uint256 indexed articleId, address indexed submitter, bytes32 contentHash);
@@ -37,6 +38,7 @@ contract NewsStaking{
         articleStakeAmount = _articleStake;
         verificationStakeAmount = _verificationStake;
         disputeStakeAmount = _disputeStake;
+        owner = msg.sender;
     }
 
     // Function to stake and submit an article
@@ -44,7 +46,7 @@ contract NewsStaking{
         require(pressToken.transferFrom(msg.sender, address(this), articleStakeAmount), "Stake transfer failed");
         
         uint256 articleId = _nextArticleId++;
-        articles[articleId] = Content(msg.sender, contentHash, articleStakeAmount, false, articleId);
+        articles[articleId] = Content(msg.sender, contentHash, articleStakeAmount, false,articleId);
         
         emit ArticleSubmitted(articleId, msg.sender, contentHash);
     }
@@ -85,5 +87,115 @@ contract NewsStaking{
         emit DisputeSubmitted(disputeId, msg.sender, contentHash, verificationId);
     }
 
-    // Additional functions to resolve articles, verifications, disputes, and to distribute rewards or penalties
+     // A mapping to keep track of the claimable amount for each address
+    mapping(address => uint256) private claimableTokens;
+
+    // Function to set tokens as claimable for a user
+    function setClaimable(address user, uint256 amount) internal {
+        // Set the amount that can be claimed by the user
+        claimableTokens[user] = amount;
+
+        // Emit an event for transparency
+        emit TokensClaimable(user, amount);
+    }
+
+    // Users call this function to claim their tokens
+    function claimTokens() external {
+        uint256 amount = claimableTokens[msg.sender];
+        require(amount > 0, "No tokens to claim");
+
+        // Before transferring, set the claimable amount to 0 to prevent re-entrancy attacks
+        claimableTokens[msg.sender] = 0;
+
+        require(pressToken.transfer(msg.sender, amount), "Token transfer failed");
+
+        // Emit an event that tokens have been claimed
+        emit TokensClaimed(msg.sender, amount);
+    }
+
+    // Event to announce tokens are claimable
+    event TokensClaimable(address indexed user, uint256 amount);
+
+    // Event to announce tokens have been claimed
+    event TokensClaimed(address indexed user, uint256 amount);
+
+    // Function to resolve a dispute and apply penalties or rewards
+    function resolveDispute(uint256 disputeId, bool isValid) external {
+        // Require that the caller is the owner
+        require(msg.sender == owner, "Unauthorized");
+
+        // Ensure the dispute exists and is not already resolved
+        Content storage dispute = disputes[disputeId];
+        require(dispute.stake > 0, "Dispute does not exist");
+        require(dispute.resolved != true, "Dispute already resolved");
+
+        if (isValid) {
+            setClaimable(dispute.submitter, dispute.stake);
+        } else {
+            // Logic to confiscate the stake
+            // You could transfer the stake to the contract owner, a rewards pool, or burn it
+        }
+
+        // Update the dispute state to resolved
+        dispute.resolved = true;
+
+        // Emit an event to record the resolution
+        emit DisputeResolved(disputeId, isValid);
+    }
+    // Event for dispute resolution
+    event DisputeResolved(uint256 indexed disputeId, bool isValid);
+
+       // Function to resolve a dispute and apply penalties or rewards
+    function resolveVerification(uint256 verificationId, bool isValid) external {
+        // Require that the caller is the owner
+        require(msg.sender == owner, "Unauthorized");
+
+        // Ensure the dispute exists and is not already resolved
+        Content storage verification = verifications[verificationId];
+        require(verification.stake > 0, "Dispute does not exist");
+        require(verification.resolved != true, "Dispute already resolved");
+
+        if (isValid) {
+            setClaimable(verification.submitter, verification.stake);
+        } else {
+            // Logic to confiscate the stake
+            // You could transfer the stake to the contract owner, a rewards pool, or burn it
+        }
+
+        // Update the dispute state to resolved
+        verification.resolved = true;
+
+        // Emit an event to record the resolution
+        emit VerificationResolved(verificationId, isValid);
+    }
+    // Event for dispute resolution
+    event VerificationResolved(uint256 indexed verificationId, bool isValid);
+
+    // Function to withdraw the stake for an article
+    function withdrawArticleStake(uint256 articleId) external {
+        Content storage article = articles[articleId];
+
+        // Ensure that the caller is the submitter of the article
+        require(msg.sender == article.submitter, "Only the submitter can withdraw the stake");
+
+        // Check that the article is not in a locked state
+        require(article.resolved != false, "Stake is currently locked");
+
+        // Ensure that the stake is greater than 0
+        uint256 stakeAmount = article.stake;
+        require(stakeAmount > 0, "No stake to withdraw");
+
+        // Update the article's stake to 0 before transferring to prevent re-entrancy attacks
+        article.stake = 0;
+
+        // Transfer the stake back to the submitter
+        require(pressToken.transfer(msg.sender, stakeAmount), "Stake transfer failed");
+
+        // Emit an event for the withdrawal
+        emit ArticleStakeWithdrawn(articleId, msg.sender, stakeAmount);
+    }
+
+    // Event for withdrawing article stake
+    event ArticleStakeWithdrawn(uint256 indexed articleId, address indexed submitter, uint256 amount);
+
 }
--- test/test2.js
+++ test/test2.js
@@ -74,5 +74,118 @@ contract('NewsStaking', function ([deployer, author, verifier, disputant, otherA
     assert.equal(dispute.referenceId.toString(), verificationId.toString(), 'The verification ID reference is incorrect');
   });
 
+  async function resolveAndClaim(verificationId, isValid, resolver, claimant) {
+    // Resolve the verification as valid or invalid
+    await newsStaking.resolveVerification(verificationId, isValid, { from: resolver });
+
+    // After resolving, the claimant should be able to claim their tokens if the resolution was in their favor
+    const initialBalance = await pressToken.balanceOf(claimant);
+    await newsStaking.claimTokens({ from: claimant });
+    const finalBalance = await pressToken.balanceOf(claimant);
+
+    // Calculate the expected balance after claiming tokens
+    const expectedBalance = isValid ? initialBalance.add(new web3.utils.BN(verificationStakeAmount)) : initialBalance;
+
+    // Verify that the claimant's token balance is updated correctly
+    assert.equal(finalBalance.toString(), expectedBalance.toString(), "Tokens were not claimed correctly");
+  }
+
+  it('should allow the owner to resolve a verification and the submitter to claim their stake', async function () {
+    const contentHash = web3.utils.sha3('Verification Content');
+    const articleId = 0; // assuming the first article has ID 0
+    
+    // Author submits an article
+    await newsStaking.submitArticle(web3.utils.sha3('Article Content'), { from: author });
+    // Verifier submits a verification
+    await newsStaking.submitVerification(articleId, contentHash, { from: verifier });
+
+    // The owner resolves the verification as valid
+    const verificationId = 0; // assuming the first verification has ID 0
+    await resolveAndClaim(verificationId, true, deployer, verifier);
+  });
+
+  it('should fail if a non-owner tries to resolve a verification', async function () {
+    const contentHash = web3.utils.sha3('Verification Content');
+    const articleId = 0; // assuming the first article has ID 0
+    
+    // Author submits an article
+    await newsStaking.submitArticle(web3.utils.sha3('Article Content'), { from: author });
+    // Verifier submits a verification
+    await newsStaking.submitVerification(articleId, contentHash, { from: verifier });
+
+    // Attempt to resolve the verification as a non-owner
+    const verificationId = 0; // assuming the first verification has ID 0
+    await expectRevert(
+      newsStaking.resolveVerification(verificationId, true, { from: verifier }),
+      'Unauthorized'
+    );
+  });
+
+  // Utility function to resolve a verification and claim tokens, extended to claim stake
+  async function resolveAndClaim(verificationId, isValid, resolver, claimant) {
+    // Resolve the verification as valid or invalid
+    await newsStaking.resolveVerification(verificationId, isValid, { from: resolver });
+
+    // Get the initial balance of the claimant
+    const initialBalance = await pressToken.balanceOf(claimant);
+
+    // Claim the tokens set as claimable
+    await newsStaking.claimTokens({ from: claimant });
+
+    // Claim back the original stake (if the resolution was successful)
+    if (isValid) {
+      await newsStaking.withdrawArticleStake(verificationId, { from: claimant });
+    }
+
+    // Get the final balance of the claimant
+    const finalBalance = await pressToken.balanceOf(claimant);
+
+    // Calculate the expected balance after claiming tokens and the original stake
+    let expectedBalance = initialBalance;
+    if (isValid) {
+      expectedBalance = expectedBalance.add(new web3.utils.BN(verificationStakeAmount));
+    }
+
+    // Verify that the claimant's token balance is updated correctly
+    assert.equal(finalBalance.toString(), expectedBalance.toString(), "Tokens were not claimed correctly");
+  }
+
+  // Add the actual test for claiming both claimable tokens and the stake after a successful resolution
+  it('should allow the verifier to claim their stake and tokens after a successful resolution', async function () {
+    const contentHash = web3.utils.sha3('Verification Content');
+    const articleId = 0; // assuming the first article has ID 0
+    
+    // Author submits an article
+    await newsStaking.submitArticle(web3.utils.sha3('Article Content'), { from: author });
+
+    // Verifier submits a verification
+    await newsStaking.submitVerification(articleId, contentHash, { from: verifier });
+
+    // The owner resolves the verification as valid
+    const verificationId = 0; // assuming the first verification has ID 0
+    await resolveAndClaim(verificationId, true, deployer, verifier);
+  });
+
+  it('should allow the owner to resolve a verification and the submitter to claim their stake', async function () {
+    const articleContentHash = web3.utils.sha3('Article Content');
+    const verificationContentHash = web3.utils.sha3('Verification Content');
+  
+    // Author submits an article
+    await newsStaking.submitArticle(articleContentHash, { from: author });
+  
+    // The owner resolves the verification as valid
+    const verificationId = 0; // assuming the first verification has ID 0
+    await newsStaking.resolveVerification(verificationId, true, { from: deployer });
+  
+    // The author (who is the submitter) claims their stake
+    const initialBalance = await pressToken.balanceOf(author);
+    await newsStaking.withdrawArticleStake(verificationId, { from: author });
+    const finalBalance = await pressToken.balanceOf(author);
+  
+    const expectedBalance = initialBalance.add(new web3.utils.BN(articleStakeAmount));
+    assert.equal(finalBalance.toString(), expectedBalance.toString(), "Author was not able to claim their stake");
+  });
+  
+
   // You can add more tests to check for failure cases and other functionality.
 });
