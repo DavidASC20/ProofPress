d66e7863885ee5b1d9f03d2c2c24f4418800a138
dlin04 <derricklin22xc@gmail.com>
2023-12-25 16:26:39 -0500

    user can add posts but they have to give their eth address, will modify to use the local storage feature

--- client/src/pages/create_post.js
+++ client/src/pages/create_post.js
@@ -1,27 +1,65 @@
 import { useState } from "react";
+import axios from 'axios';
+// import { useCookies } from 'react-cookie';
+// import { useNavigate } from 'react-router-dom';
 
 export const CreatePosts = () => {
-  const [post, registerPost] = useState({
-    poster_address: "",
-    post_content: "",
-  });
-
-  // const handleChange = () => {
-  //   const {name, value} = event.target;
-  //   registerPost({...post, [name]: value});
-  // }
-
   return(
-    <div className="create_post">
-      <h2> Create Post </h2>
-      <form>
-        <label htmlFor="poster_address">  </label>
-        <input type="text" id="poster_address" name="poster_address"/>
-
-        <label htmlFor="post_content">  </label>
-        <input type="text" id="post_content" />
-        
-      </form>
+    <div className="create-post">
+      <CreatePost />
     </div>
   );
+}
+
+const CreatePost = () => {
+  const [ethereum_address, setEthereumAddress] = useState("");
+  const [post_content, setPostContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // sends posters ethereum address and post content to backend to save to db
+      const response = await axios.post("http://localhost:3001/create-posts/create", {ethereum_address, post_content});
+      console.log(response);
+    } catch(err) {
+     console.error(err);
+    }
+  }
+
+  return (
+    <Form
+      ethereum_address={ethereum_address}
+      setEthereumAddress={setEthereumAddress}
+      post_content={post_content}
+      setPostContent={setPostContent}
+      onSubmit={onSubmit}
+    />
+  )
+}
+
+const Form = ({ethereum_address, setEthereumAddress, post_content, setPostContent, onSubmit}) => {
+  return(
+    <div className="creation-container">
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="ethereum-address">Ethereum_Address: </label>
+          <input 
+            type="text"
+            id="ethereum-address"
+            value={ethereum_address}
+            onChange={(event) => setEthereumAddress(event.target.value)}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="post-content">Post Content: </label>
+          <input
+            type="text"
+            id="post-content"
+            value={post_content}
+            onChange={(event) => setPostContent(event.target.value)}/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
