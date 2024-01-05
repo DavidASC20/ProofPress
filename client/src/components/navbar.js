11dc5f1b5f7028280d63e01867a28b42ec8545ae
dlin04 <derricklin22xc@gmail.com>
2024-01-05 14:11:16 -0500

    change auth.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 export const Navbar = () => {
+  const userAddress = localStorage.getItem('userAddress');
+
   return(
-    // change the MyProfile to Login / MyProfile by checking to session when user is logged in
     <div>
       <Container className="d-flex align-items-center justify-content-center">
         <Link to="/" className="mx-3"> Home </Link>
         <Link to="/how-to" className="mx-3"> Getting Started </Link>
         <Link to="/create-posts" className="mx-3"> Create Posts </Link>
-        <Link to="/auth" className="mx-3"> My Profile </Link>
+        {userAddress ? (
+          <Link to="/logout" className="mx-3"> Logout </Link>
+        ) : (
+          <Link to="/auth" className="mx-3"> Login </Link>
+        )}
       </Container>
     </div>
   );
--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,80 +1,76 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Web3 from 'web3';
 
 export const Auth = () => {
   return (
-    <div>
-      temp auth
-      <p>this page will handle client side authentication with metamask
-      and send data to the backend to create a user or check if user exists</p>
+    <div className="auth">
+      <Login />
     </div>
-  )
-}
-// import Web3 from 'web3';
+  );
+};
 
-// export const Auth = () => {
-//   return (
-//     <div className="auth">
-//       <Login />
-//     </div>
-//   );
-// };
+const Login = () => {
+  const [userAddress, setUserAddress] = useState('');
+  const [isMetaMaskInstalled, setIsMetaMaskInstalled] = useState(false);
 
-// const Login = () => {
-//   const [userAddress, setUserAddress] = useState('');
-//   const [isMetaMaskInstalled, setIsMetaMaskInstalled] = useState(false);
+  useEffect(() => {
+    // Check if MetaMask is installed
+    if (window.ethereum) {
+      setIsMetaMaskInstalled(true);
+      const address = localStorage.getItem('userAddress');
+      if (address) {
+        setUserAddress(address);
+      }
+    }
+  }, []);
 
-//   useEffect(() => {
-//     // Check if MetaMask is installed
-//     if (window.ethereum) {
-//       setIsMetaMaskInstalled(true);
-//       const address = localStorage.getItem('userAddress');
-//       if (address) {
-//         setUserAddress(address);
-//       }
-//     }
-//   }, []);
+  const connectToMetaMask = async (event) => {
+    event.preventDefault();
+    if (isMetaMaskInstalled) {
+      try {
+        await window.ethereum.enable();
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        setUserAddress(accounts[0]);
+        localStorage.setItem('userAddress', accounts[0]); // Save to localStorage
 
-//   const connectToMetaMask = async () => {
-//     if (isMetaMaskInstalled) {
-//       try {
-//         await window.ethereum.enable();
-//         const web3 = new Web3(window.ethereum);
-//         const accounts = await web3.eth.getAccounts();
-//         setUserAddress(accounts[0]);
-//         localStorage.setItem('userAddress', accounts[0]); // Save to localStorage
-//       } catch (error) {
-//         console.error('MetaMask connection error:', error);
-//       }
-//     } else {
-//       console.error('MetaMask not found in your browser.');
-//     }
-//   }
+        // send address to database in the backend
+        const response = await axios.post("http://localhost:3001/auth-users/login", {ethereum_address: accounts[0]});
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.error("metamask not found in browser");
+    }
+  }
   
 
-//   const disconnectFromMetaMask = () => {
-//     // Clear user-related data or tokens
-//     setUserAddress('');
-//     localStorage.removeItem('userAddress'); // Remove from localStorage
-//   }
+  const disconnectFromMetaMask = () => {
+    // Clear user-related data or tokens
+    setUserAddress('');
+    localStorage.removeItem('userAddress'); // Remove from localStorage
+  }
   
 
-//   return (
-//     <div className="login">
-//       <h2>Login with MetaMask</h2>
-//       {isMetaMaskInstalled ? (
-//         <div>
-//           {userAddress ? (
-//             <div>
-//               <button onClick={disconnectFromMetaMask}>Disconnect</button>
-//               <p>Connected Ethereum Address: {userAddress}</p>
-//             </div>
-//           ) : (
-//             <button onClick={connectToMetaMask}>Connect to MetaMask</button>
-//           )}
-//         </div>
-//       ) : (
-//         <p>MetaMask is not installed in your browser.</p>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div className="login">
+      <h2>Login with MetaMask</h2>
+      {isMetaMaskInstalled ? (
+        <div>
+          {userAddress ? (
+            <div>
+              <button onClick={disconnectFromMetaMask}>Disconnect</button>
+              <p>Connected Ethereum Address: {userAddress}</p>
+            </div>
+          ) : (
+            <button onClick={connectToMetaMask}>Connect to MetaMask</button>
+          )}
+        </div>
+      ) : (
+        <p>MetaMask is not installed in your browser.</p>
+      )}
+    </div>
+  );
+};
